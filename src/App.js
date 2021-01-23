14ba607c3173b5908612f400b487d939047f3067
Hamed Kaff <s3708483@student.rmit.edu.au>
2021-01-23 17:01:39 +1100

    Setting Up React, Project Clean Up, Basic Start Up Structure and Files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+function App() {
+  return (
+    <div className="app">
+      <h1> COVID-19 Live Tracker</h1> 
+
+      {/*STRUCTURE*/}
+
+        {/* HEADER */}
+          {/* TITLE + LOGO */}
+          {/* HOME PAGE */}
+          {/* WHAT IS COVID PAGE */}
+          {/* ANALYTICS PAGE */}
+          {/* ABOUT PAGE */}
+          {/* CONTACT PAGE */}
+
+        {/* 3 DATA BOXES */}
+
+        {/* 1+ GRAPH */}
+
+        {/* LIST TABLE */}
+
+        {/* MAP */}
+
+
+
+    </div>
+  );
+}
+
+export default App;
